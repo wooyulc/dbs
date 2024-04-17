d73c6b81d592fd64ab2740dd8ad57d91982256c2
Wooyul Chung <wooyulc@uci.edu>
2024-04-17 21:49:07 +0900

    Updates

--- gatsby-node.js
+++
@@ -1,9 +0,0 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/",
-    component: require.resolve("./src/templates/index.js"),
-    context: {},
-    defer: true,
-  })
-}
